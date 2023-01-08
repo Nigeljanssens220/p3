7a0c902e3b2102bd9662802b69fb4c31e910ccfc
nigeljanssens <njanssens@inlichtingenbureau.nl>
2023-01-08 11:27:40 +0100

    wedstrijvenoverizcht responsiveness

--- src/components/Tabs/Panels/Wedstrijden.tsx
+++ src/components/Tabs/Panels/Wedstrijden.tsx
@@ -4,18 +4,10 @@ import GameCard from "../../GameCard";
 import { GameWithPlayers } from "../../types/game";
 
 const Wedstrijden: React.FC = ({}) => {
-  const { data, isLoading, isError, error } = api.game.getAll.useQuery(
-    undefined,
-    {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchInterval: 0,
-    }
-  );
+  const { data, isLoading, isError, error } = api.game.getAll.useQuery();
 
   return (
-    <section className="grid w-full grid-cols-2 gap-4 text-gray-100 md:grid-cols-3 lg:grid-cols-4">
+    <section className="grid w-full gap-4 text-gray-100 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {isLoading && (
         <div className="col-span-4 flex items-center justify-center">
           <DotSpinner color="white" />
