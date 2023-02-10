92fe8c3708c647ebdb1fd1e48b546caa93d333e9
Nigel Anthony <61184181+Nigeljanssens220@users.noreply.github.com>
2023-02-10 16:09:00 +0100

    refactor tabs and fix player rankings (#7)
    * player table
    
    * worst player
    
    ---------
    
    Co-authored-by: nigeljanssens <njanssens@inlichtingenbureau.nl>

--- src/components/Tabs/Panels/Games.tsx
+++ src/components/Tabs/Panels/Games.tsx
@@ -1,4 +1,5 @@
 import { DotSpinner } from "@uiball/loaders";
+import { TabsContent } from "..";
 import { api } from "../../../utils/api";
 import GameCard from "../../GameCard";
 import { GameWithPlayers } from "../../types/game";
@@ -7,16 +8,18 @@ const Games: React.FC = ({}) => {
   const { data, isLoading, isError, error } = api.game.getAll.useQuery();
 
   return (
-    <section className="grid w-full gap-4 text-gray-100 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {isLoading && (
-        <div className="col-span-4 flex items-center justify-center">
-          <DotSpinner color="white" />
-        </div>
-      )}
-      {data?.map((game) => (
-        <GameCard key={game.id} data={game as GameWithPlayers} />
-      ))}
-    </section>
+    <TabsContent value="games">
+      <section className="grid w-full gap-4 text-gray-100 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {isLoading && (
+          <div className="col-span-4 flex items-center justify-center">
+            <DotSpinner color="white" />
+          </div>
+        )}
+        {data?.map((game) => (
+          <GameCard key={game.id} data={game as GameWithPlayers} />
+        ))}
+      </section>
+    </TabsContent>
   );
 };
 
--- src/components/Tabs/Panels/PlayerScores.tsx
+++ src/components/Tabs/Panels/PlayerScores.tsx
@@ -1,18 +1,50 @@
+import { FaceFrownIcon, FaceSmileIcon } from "@heroicons/react/20/solid";
+import { TabsContent } from "..";
 import { api } from "../../../utils/api";
-import PlayerCard from "../../PlayerCard";
+import classNames from "../../../utils/styling";
+import Typography from "../../Typography";
 
 const PlayerScores: React.FC = ({}) => {
   const allPlayers = api.player.getAll.useQuery().data;
 
   return (
-    <section className="grid w-full gap-4 text-gray-100 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      <>
-        {allPlayers &&
-          allPlayers.map((player) => (
-            <PlayerCard key={player.id} data={player} />
-          ))}
-      </>
-    </section>
+    <TabsContent value="playerScore" className="max-w-sm">
+      <section className=" flex w-full max-w-sm flex-col gap-4 space-y-2 rounded-lg bg-gray-100 bg-opacity-25 bg-clip-padding p-4 px-20 text-gray-100 backdrop-blur-3xl backdrop-filter">
+        <ul className="list-decimal divide-y">
+          {allPlayers &&
+            allPlayers.map((player, idx) => {
+              const iconClassName =
+                idx === 0
+                  ? "text-yellow-400"
+                  : idx === 1
+                  ? "text-gray-400"
+                  : idx === 2
+                  ? "text-red-400"
+                  : "";
+
+              return (
+                <li key={player.id}>
+                  <div className="relative grid grid-cols-2 items-center gap-8 ">
+                    {!!iconClassName && (
+                      <FaceSmileIcon
+                        className={classNames(
+                          iconClassName,
+                          "absolute -left-14 h-5 w-5"
+                        )}
+                      />
+                    )}
+                    {allPlayers.length === idx + 1 && (
+                      <FaceFrownIcon className="absolute -left-14 h-5 w-5 text-red-900" />
+                    )}
+                    <Typography>{player.name}</Typography>
+                    <Typography>{player.eloRating}</Typography>
+                  </div>
+                </li>
+              );
+            })}
+        </ul>
+      </section>
+    </TabsContent>
   );
 };
 
