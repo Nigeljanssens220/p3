6024a5eb4c0e3abe61ff1a0bc2a264e91c461d6a
nigeljanssens <njanssens@inlichtingenbureau.nl>
2023-02-10 16:08:14 +0100

    worst player

--- src/components/Tabs/Panels/PlayerScores.tsx
+++ src/components/Tabs/Panels/PlayerScores.tsx
@@ -1,4 +1,4 @@
-import { FaceSmileIcon } from "@heroicons/react/20/solid";
+import { FaceFrownIcon, FaceSmileIcon } from "@heroicons/react/20/solid";
 import { TabsContent } from "..";
 import { api } from "../../../utils/api";
 import classNames from "../../../utils/styling";
@@ -9,8 +9,8 @@ const PlayerScores: React.FC = ({}) => {
 
   return (
     <TabsContent value="playerScore" className="max-w-sm">
-      <section className=" flex w-full max-w-sm flex-col gap-4 space-y-2 rounded-lg bg-gray-100 bg-opacity-25 bg-clip-padding p-4 px-12 text-gray-100 backdrop-blur-3xl backdrop-filter">
-        <ul className="divide-y">
+      <section className=" flex w-full max-w-sm flex-col gap-4 space-y-2 rounded-lg bg-gray-100 bg-opacity-25 bg-clip-padding p-4 px-20 text-gray-100 backdrop-blur-3xl backdrop-filter">
+        <ul className="list-decimal divide-y">
           {allPlayers &&
             allPlayers.map((player, idx) => {
               const iconClassName =
@@ -29,9 +29,12 @@ const PlayerScores: React.FC = ({}) => {
                       <FaceSmileIcon
                         className={classNames(
                           iconClassName,
-                          "absolute -left-8 mr-4 h-5 w-5"
+                          "absolute -left-14 h-5 w-5"
                         )}
                       />
+                    )}
+                    {allPlayers.length === idx + 1 && (
+                      <FaceFrownIcon className="absolute -left-14 h-5 w-5 text-red-900" />
                     )}
                     <Typography>{player.name}</Typography>
                     <Typography>{player.eloRating}</Typography>
